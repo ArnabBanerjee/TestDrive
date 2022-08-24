081ef9ec9190d335765def4484124b66a30032c6
Arnab Banerjee <arnab_banerjee@yahoo.com>
2022-08-25 01:19:18 +0530

    feat: assignment 1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as path from 'path';
 /**
  * Assignment 2. Counter Function
  * Implement the `counter` function according to requirements:
@@ -34,3 +36,37 @@ const c = counter();
 console.log(c[0]()); c[1]();
 console.log(c[0]()); c[1]();
 console.log(c[0]()); c[1]();
+
+/**
+ * Assignment 1. HTTP Request Handling
+    Write down code snippet using following requirements:
+    - Use only vanilla TypeScript, without additional libraries.
+    - Use REST Rick and Morty API(https://rickandmortyapi.com/documentation/#rest) for this assignment.
+    - Request all episodes using API.
+    - Replace URLs in “characters” array with character JSON objects taken from API.
+    - Log final array into console.
+ */
+
+export const episodesFetcher = async () => {
+    try {
+        const baseUrl = 'https://rickandmortyapi.com/api';
+        const allEpisodes = await fetch(`${baseUrl}/episode`).then(r => r.json()).catch(e => console.error(e.message));
+        const results = allEpisodes.results;
+        for (let index = 0; index < results.length; index++) {
+            const element = results[index];
+            const characters = element.characters;
+            const newCharecters = [];
+            for (let i = 0; i < characters.length; i++) { 
+                const charecter = await fetch(characters[i]).then(r => r.json()).catch(e => console.error(e.message));
+                newCharecters.push(charecter)
+            }
+            allEpisodes.results[index].characters = newCharecters;
+        }
+        const outputPath = path.join(__dirname, '..', 'tmp');
+        fs.mkdirSync(outputPath)
+        fs.writeFileSync(path.join(outputPath, 'output.json'), JSON.stringify(allEpisodes, null, 4), 'utf8');
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+episodesFetcher();
